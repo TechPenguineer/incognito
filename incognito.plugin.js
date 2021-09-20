bf956fc9b9e2c3eebd928728ba11e75fdcf897ec
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-19 21:58:22 -0400

    added manifest

---
+++ incognito.plugin.js
@@ -0,0 +1,9 @@
+/**
+ * @name incognito
+ * @author TechPenguineer
+ * @authorId 692051505188044850
+ * @version 0.1.0
+ * @description Open links from discord in an incognito tab
+ * @source https://github.com/TechPenguineer/incognito
+ * @updateUrl 
+ */
