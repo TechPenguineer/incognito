aeeac313ec44e0ee1ae5439fd3d43ee3f3905c32
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-19 22:10:17 -0400

    Update incognito.plugin.js

--- incognito.plugin.js
+++ incognito.plugin.js
@@ -2,7 +2,7 @@
  * @name incognito
  * @author TechPenguineer
  * @authorId 692051505188044850
- * @version 0.1.0
+ * @version 0.1.0.1
  * @description Open links from discord in an incognito tab
  * @source https://github.com/TechPenguineer/incognito
  * @updateUrl https://github.com/TechPenguineer/incognito/blob/main/incognito.plugin.js
