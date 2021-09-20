5f0133e9f9b1b7025cfec7564b34bec0953e8227
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-19 22:02:48 -0400

    added config options

--- incognito.plugin.js
+++ incognito.plugin.js
@@ -5,5 +5,30 @@
  * @version 0.1.0
  * @description Open links from discord in an incognito tab
  * @source https://github.com/TechPenguineer/incognito
- * @updateUrl 
+ * @updateUrl https://github.com/TechPenguineer/incognito/blob/main/incognito.plugin.js
  */
+
+module.exports = (_=>
+    {
+        const config = 
+        {
+            "info": 
+            {
+                "name":"incognito",
+                "author": "TechPenguineer",
+                "version": "0.1.0",
+                "description": "Open links from discord in an incognito tab"
+            },
+            "changeLog": 
+            {
+                "added":
+                {
+                    "Manifest ": "Config manifest",
+                    "Boilerplate": "Added plugin boiler",
+                    "Structure": "Made plugin structure",
+                    "Initial Release": "Published v1 & git repo"
+                    
+                }
+            }
+        }        
+    })
